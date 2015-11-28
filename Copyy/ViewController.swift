5f724440be73542e6a180726d7e42f0948f8c336
Theodore <theosidebotham@me.com>
2015-11-28 17:44:25 +0000

    Initial Commit

---
+++ Copyy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Copyy
+//
+//  Created by Theo Sidebotham on 28/11/2015.
+//  Copyright © 2015 charles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
